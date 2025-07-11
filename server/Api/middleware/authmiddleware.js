be2650dcb1a54cb2b97f2d15ef807c083909c53a
unknown <vineelareddy925@gmail.com>
2025-07-11 15:02:47 +0530

    vinni deployed

--- server/Api/middleware/authmiddleware.js
+++ server/Api/middleware/authmiddleware.js
@@ -22,23 +22,23 @@
 
 // module.exports = { protect };
 // middleware/authMiddleware.js
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+// const authMiddleware = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'No token provided' });
-  }
+//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
+//     return res.status(401).json({ message: 'No token provided' });
+//   }
 
-  try {
-    const token = authHeader.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
-};
+//   try {
+//     const token = authHeader.split(' ')[1];
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch (err) {
+//     return res.status(401).json({ message: 'Invalid token' });
+//   }
+// };
 
-module.exports = authMiddleware;
+// module.exports = authMiddleware;
